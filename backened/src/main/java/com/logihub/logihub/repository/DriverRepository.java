0ba670cbe27be5371ce45d7dc6eefa91695dcab3
104piyush <mahajanpiyush494@gmail.com>
2025-10-18 19:52:01 +0530

    driver repo ,403 solve

--- backened/src/main/java/com/logihub/logihub/repository/DriverRepository.java
+++ backened/src/main/java/com/logihub/logihub/repository/DriverRepository.java
@@ -1,11 +1,15 @@
 package com.logihub.logihub.repository;
 
 import com.logihub.logihub.entity.Driver;
+import com.logihub.logihub.entity.PasswordResetToken;
+import com.logihub.logihub.entity.User;
 import com.logihub.logihub.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DriverRepository extends JpaRepository<Driver,Long> {
-    List<Vehicle> findByTransporterId(Long transporterId);
+    List<Driver> findByTransporterId(Long transporterId);
+
 }
