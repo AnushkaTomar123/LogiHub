ed74c7214ea7b4f55c608d2fea0e88ac3da0acc6
AnushkaTomar123 <tomaranushka885@gmail.com>
2025-10-19 14:19:17 +0530

    resolve conflicts

--- backened/src/main/java/com/logihub/logihub/repository/DriverRepository.java
+++ backened/src/main/java/com/logihub/logihub/repository/DriverRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DriverRepository extends JpaRepository<Driver,Long> {
-    List<Vehicle> findByTransporterId(Long transporterId);
+    List<Driver> findByTransporterId(Long transporterId);
 }
